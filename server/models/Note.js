fd14317f537133cc18a29622600b8bcb2985b6f2
Riley Tudbury <tudbury@gmail.com>
2020-02-21 11:53:02 -0700

    bug and note schema in place

---
+++ server/models/Note.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const objectId = mongoose.Schema.Types.ObjectId
+
+const Note = new Schema(
+  {
+    content: { type: String, required: true },
+    bug: { type: objectId, ref: "Bug", required: true },
+    reportedBy: { type: String, required: true }
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+);
+
+export default Note;
